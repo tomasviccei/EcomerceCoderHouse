074120dddd5a38617f11ca20fefac11174c88312
Tomas <tomasviccei25@gmail.com>
2023-12-06 23:23:06 -0300

    Entrega final - Tomas Viccei

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -7,7 +7,7 @@ import "./css/ItemList.css";
 
 export const Item = ({ item }) => {
   return (
-    <Card style={{ width: "18rem" }}>
+    <Card >
       <Card.Img variant="top" height={320} src={item.pictureUrl} />
       <Card.Body className="card-body">
         <Card.Title style={{ color: "#fff", fontSize: "25px" }}>
@@ -15,7 +15,7 @@ export const Item = ({ item }) => {
         </Card.Title>
         <Card.Text style={{ color: "#fff" }}>{item.description}</Card.Text>
         <Link to={`/items/${item.id}`}>
-          <Button variant="secondary" className="boton">Saber Mas</Button>
+          <button  className="btn-ir">Saber Mas</button>
         </Link>
       </Card.Body>
     </Card>
